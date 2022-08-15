1530c3a8ebee637daf0da2b6f17523f0f9638c47
Manav Misra <manavm@visionwebsoft.com>
2022-08-15 06:25:30 -0500

    Add solution for blank strings

--- features/step_definitions/steps.mjs
+++ features/step_definitions/steps.mjs
@@ -1,10 +1,12 @@
-import { Given, When, Then } from "@cucumber/cucumber";
-import tryConvert from "../../app/index.js";
+import { Given, Then, When } from "@cucumber/cucumber";
 import { expect } from "expect";
+import tryConvert from "../../app/index.js";
 
 Given("I have a temperature of {float}° {units}", function (temp2Convert, _) {
   this.temp2Convert = temp2Convert;
 });
+
+Given("I have non-numerical input", () => {});
 
 When("I convert it to degrees {units}", function (units) {
   this.convertedTemp = tryConvert(this.temp2Convert, units);
@@ -13,6 +15,14 @@ When("I convert it to degrees {units}", function (units) {
 Then("I get/see {float}° {units}", function (expectedTemp, _) {
   const expectHelper = () => {
     expect(this.convertedTemp).toEqual(expectedTemp.toString());
+  };
+
+  expectHelper();
+});
+
+Then('I "see" a blank string', function () {
+  const expectHelper = () => {
+    expect(this.convertedTemp).toBe("");
   };
 
   expectHelper();
